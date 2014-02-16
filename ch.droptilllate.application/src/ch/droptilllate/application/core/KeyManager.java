9fe97e66d253427cca6077dab250aee137e79d22
dev131 <betschart.ma@gmail.com>
2014-02-16 11:10:00 +0000

    Version 0.3.2

--- ch.droptilllate.application/src/ch/droptilllate/application/core/KeyManager.java
+++ ch.droptilllate.application/src/ch/droptilllate/application/core/KeyManager.java
@@ -1,16 +1,7 @@
 package ch.droptilllate.application.core;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
 
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.XMLReaderFactory;
-
+import ch.droptilllate.application.com.IXmlDatabase;
 import ch.droptilllate.application.dao.ShareFolderDao;
 import ch.droptilllate.application.dnb.ShareFolder;
 import ch.droptilllate.application.views.Messages;
@@ -23,7 +14,7 @@ public class KeyManager {
 	 */
 	public void initPassword(String password, String salt){
 		KeysGenerator kg = new KeysGenerator();
-		ShareFolderDao dao = new ShareFolderDao();
+		IXmlDatabase dao = new ShareFolderDao();
 		ShareFolder	folder = new ShareFolder(0, Messages.getLocalPathDropBoxMaster(), kg.getKey(password, salt));
 		dao.newElement(folder);
 	}
@@ -33,7 +24,7 @@ public class KeyManager {
 	 * @return true if exist
 	 */
 	public boolean checkMasterPasswordExisting(){
-		ShareFolderDao dao = new ShareFolderDao();
+		IXmlDatabase dao = new ShareFolderDao();
 		ShareFolder folder = (ShareFolder) dao.getElementByID(0);
 		if(folder == null ){
 			return false;
@@ -47,7 +38,7 @@ public class KeyManager {
 	 */
 	public boolean checkPassword(String password, String salt, int shareID){
 		KeysGenerator kg = new KeysGenerator();
-		ShareFolderDao dao = new ShareFolderDao();
+		IXmlDatabase dao = new ShareFolderDao();
 		ShareFolder folder = (ShareFolder) dao.getElementByID(shareID);
 		if(folder.getKey().equals(kg.getKey(password, salt)))
 			return true;
